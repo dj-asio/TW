5009dc51f8498c076f729c683811fd6c24e22845
tasos <a.tsantakidis@csl.gr>
2025-12-30 08:43:21 +0200

    backtimerename.js -> bold noble

--- backtimerename.js
+++ backtimerename.js
@@ -1,18 +1,18 @@
-var cookieName= "UniversalBack";
+const cookieName = "UniversalBack";
 
-var WorldSpeed
-var UnitSpeed
+let WorldSpeed;
+let UnitSpeed;
 
 const domain = document.domain;
 
-var tomorrowTranslated = "tomorrow";
-var atTranslated = "at";
-var prevUnit = "noble";
-var explor = "spy";
-var attackTranslated = "Attack";
+let tomorrowTranslated = "tomorrow";
+let atTranslated = "at";
+let prevUnit = "noble";
+let explore = "spy";
+let attackTranslated = "Attack";
 
 
-var baseUnitSpeed = {
+/*var baseUnitSpeed = {
     "Noble" : 35,
     "B\351lier" : 30,
     "\311p\351e" : 22,
@@ -20,37 +20,21 @@ var baseUnitSpeed = {
     "Lourd" : 11,
     "L\351ger" : 10,
     "\311claireur" : 9,
-}
-var baseUnitSpeed = {
-    "noble" : 35,
-    "ram" : 30,
-    "sword" : 22,
-    "axe" : 18,
-    "hcav" : 11,
-    "lcav" : 10,
-    "spy" : 9,
-}
-if(domain.includes("tribalwars.com.pt") || domain.includes("tribalwars.com.br")){
-    tomorrowTranslated = "amanhã";
-    atTranslated = "às";
-    prevUnit = "nobre";
-    explor = "btd";
-    attackTranslated = "Ataque";
-    baseUnitSpeed = {
-        "nobre" : 35,
-        "aríete" : 30,
-        "espada" : 22,
-        "machado" : 18,
-        "cavp" : 11,
-        "cavl" : 10,
-        "btd" : 9,
-    }
-}
-else if(domain.includes("tribalwars.net") || domain.includes("tribalwars.co.uk")){
+}*/
+let baseUnitSpeed = {
+    "noble": 35,
+    "ram": 30,
+    "sword": 22,
+    "axe": 18,
+    "hcav": 11,
+    "lcav": 10,
+    "spy": 9,
+};
+if(domain.includes("tribalwars.net") || domain.includes("tribalwars.co.uk")){
     tomorrowTranslated = "tomorrow";
     atTranslated = "at";
     prevUnit = "noble";
-    explor = "spy";
+    explore = "spy";
     attackTranslated = "Attack";
 
     baseUnitSpeed = {
@@ -63,11 +47,11 @@ else if(domain.includes("tribalwars.net") || domain.includes("tribalwars.co.uk")
         "spy" : 9,
     }
 }
-else if(domain.includes("guerretribale.fr")){
+/*else if(domain.includes("guerretribale.fr")){
     tomorrowTranslated = "Demain";
     atTranslated = "le";
     prevUnit = "Noble";
-    explor = "\311claireur";
+    explore = "\311claireur";
     attackTranslated = "Attaque";
 
     baseUnitSpeed = {
@@ -79,46 +63,12 @@ else if(domain.includes("guerretribale.fr")){
         "L\351ger" : 10,
         "\311claireur" : 9,
     }
-}
-else if(domain.includes("triburile.ro")){
-    tomorrowTranslated = "mâine";
-    atTranslated = "la";
-    prevUnit = "GN";
-    explor = "Spionează";
-    attackTranslated = "Atac";
-
-    baseUnitSpeed = {
-        "GN" : 35,
-        "Berbec" : 30,
-        "Spadă" : 22,
-        "Topor" : 18,
-        "HCav" : 11,
-        "LCav" : 10,
-        "Spionează" : 9,
-    }
-}
-else if(domain.includes("guerrastribales.es")){
-    tomorrowTranslated = "mañana";
-    atTranslated = "a las";
-    prevUnit = "noble";
-    explor = "Espía";
-    attackTranslated = "Ataque";
-
-    baseUnitSpeed = {
-        "noble" : 35,
-        "Ariete" : 30,
-        "espada" : 22,
-        "hacha" : 18,
-        "lourde" : 11,
-        "CAVL" : 10,
-        "Espía" : 9,
-    }
-}
-else if(domain.includes("tribalwars.nl")){
+}*/
+/*else if(domain.includes("tribalwars.nl")){
     tomorrowTranslated = "morgen";
     atTranslated = "om";
     prevUnit = "Edel.";
-    explor = "Verk.";
+    explore = "Verk.";
     attackTranslated = "Aanval";
 
     baseUnitSpeed = {
@@ -130,12 +80,12 @@ else if(domain.includes("tribalwars.nl")){
         "LCav." : 10,
         "Verk." : 9,
     }
-}
+}*/
 else if(domain.includes("fyletikesmaxes.gr")){
     tomorrowTranslated = "αύριο";
     atTranslated = "στις";
-    prevUnit = "Αριστοκράτης";
-    explor = "Ανιχνευτής";
+    prevUnit = "<b>Αριστοκράτης</b>";
+    explore = "Ανιχνευτής";
     attackTranslated = "Επίθεση";
 
     baseUnitSpeed = {
@@ -148,45 +98,11 @@ else if(domain.includes("fyletikesmaxes.gr")){
         "Ανιχνευτής" : 9,
     }
 }
-else if(domain.includes("divokekmeny.cz")){
-    tomorrowTranslated = "zítřek";
-    atTranslated = "v";
-    prevUnit = "Šlechta";
-    explor = "Špeh";
-    attackTranslated = "Útok";
-
-    baseUnitSpeed = {
-        "Šlechta" : 35,
-        "Beranidlo" : 30,
-        "Meč" : 22,
-        "Sekera" : 18,
-        "TK" : 11,
-        "LK" : 10,
-        "Špeh" : 9,
-    }
-}
-else if(domain.includes("plemiona.pl")){
-    tomorrowTranslated = "jutro";
-    atTranslated = "o";
-    prevUnit = "Szlachcic";
-    explor = "Zwiad";
-    attackTranslated = "Atak";
-
-    baseUnitSpeed = {
-        "Szlachcic" : 35,
-        "Taran" : 30,
-        "Miecz" : 22,
-        "Topór" : 18,
-        "CK" : 11,
-        "LK" : 10,
-        "Zwiad" : 9,
-    }
-}
-else if(domain.includes("voynaplemyon.com")){
+/*else if(domain.includes("voynaplemyon.com")){
     tomorrowTranslated = "завтра";
     atTranslated = "om";
     prevUnit = "Дворянин";
-    explor = "Лазутчик";
+    explore = "Лазутчик";
     attackTranslated = "Атака";
 
     baseUnitSpeed = {
@@ -199,28 +115,11 @@ else if(domain.includes("voynaplemyon.com")){
         "Лазутчик" : 9,
     }
 }
-else if(domain.includes("klanhaboru.hu")){
-    tomorrowTranslated = "holnap"; // talvez n
-    atTranslated = "om";
-    prevUnit = "FN";
-    explor = "Kém";
-    attackTranslated = "Támadás";
-
-    baseUnitSpeed = {
-        "FN" : 35,
-        "Kos" : 30,
-        "Kard" : 22,
-        "Bárdos" : 18,
-        "NLov" : 11,
-        "KLov" : 10,
-        "Kém" : 9,
-    }
-}
 else if(domain.includes("tribals.it")){
     tomorrowTranslated = "domani";
     atTranslated = "alle";
     prevUnit = "nobile";
-    explor = "Esploratore";
+    explore = "Esploratore";
     attackTranslated = "Attacco";
 
     baseUnitSpeed = {
@@ -233,27 +132,10 @@ else if(domain.includes("tribals.it")){
         "Esploratore" : 9,
     }
 }
-else if(domain.includes("klanlar.org")){
-    tomorrowTranslated = "yarın şu";
-    atTranslated = "saat";
-    prevUnit = "M";
-    explor = "Casus";
-    attackTranslated = "Saldırı";
-
-    baseUnitSpeed = {
-        "M" : 35,
-        "Koçbaşı" : 30,
-        "Kılıç" : 22,
-        "Balta" : 18,
-        "AA" : 11,
-        "HA" : 10,
-        "Casus" : 9,
-    }
-}
 else if(domain.includes("tribalwars.ae")){
     atTranslated = " غدا";
     prevUnit = "نبيل";
-    explor = "كشافة";
+    explore = "كشافة";
     attackTranslated = "هجوم";
 
     baseUnitSpeed = {
@@ -265,34 +147,33 @@ else if(domain.includes("tribalwars.ae")){
         "الفرسان الخفيفة" : 10,
         "كشافة" : 9,
     }
-}
+}*/
 
 
 function getTimeLeftInSecond(row){
-    var TimeString = $(row).find('td')[6].innerText;
-    var split = TimeString.split(':');
+    const TimeString = $(row).find('td')[6].innerText;
+    const split = TimeString.split(':');
     return parseInt(split[2])+60*parseInt(split[1])+3600*parseInt(split[0]);
 }
 
 function getSender(row){
-    return $(row).find('td')[3].innerText; // TODO A Testzer
+    return $(row).find('td')[3].innerText; // TODO A Tester
 }
 
 function getDistance(row){
-    var coordAtt = getAttacker(row).split("|");
-    var coordDef = getDefender(row).split("|");
-    var exactDistance = Math.sqrt(Math.pow(parseInt(coordAtt[0])-parseInt(coordDef[0]),2) + Math.pow(parseInt(coordAtt[1])-parseInt(coordDef[1]),2));
-    return exactDistance;
+    const coordAtt = getAttacker(row).split("|");
+    const coordDef = getDefender(row).split("|");
+    return Math.sqrt(Math.pow(parseInt(coordAtt[0]) - parseInt(coordDef[0]), 2) + Math.pow(parseInt(coordAtt[1]) - parseInt(coordDef[1]), 2));
 }
 function getDefender(row){
-    var a = $(row).find('td')[1].innerText; // TODO A Testzer
-    tab = a.split(')');
+    const a = $(row).find('td')[1].innerText; // TODO A Tester
+    let tab = a.split(')');
     return tab[tab.length-2].split('(')[1];
 }
 
 function getAttacker(row){
-    var a = $(row).find('td')[2].innerText; // TODO A Testzer
-    tab = a.split(')');
+    const a = $(row).find('td')[2].innerText; // TODO A Tester
+    let tab = a.split(')');
     return tab[tab.length-2].split('(')[1];
 }
 
@@ -301,29 +182,29 @@ function getTravelTimeInSecond(distance, unit){
 }
 
 function getBackTime(row){
-    var impact = conversionImpacttoDate(row);
-    var travel = getTravelTimeInSecond(getDistance(row), findAttackSpeed(row));
-    var launchDate = new Date(impact.getFullYear(), impact.getMonth(), impact.getDate(), impact.getHours(), impact.getMinutes(), parseInt(impact.getSeconds())+travel);
-    var dateStringSplit = launchDate.toString().split(" ");
+    const impact = conversionImpactDate(row);
+    const travel = getTravelTimeInSecond(getDistance(row), findAttackSpeed(row));
+    const launchDate = new Date(impact.getFullYear(), impact.getMonth(), impact.getDate(), impact.getHours(), impact.getMinutes(), parseInt(impact.getSeconds()) + travel);
+    const dateStringSplit = launchDate.toString().split(" ");
     return dateStringSplit[1] + ". " + dateStringSplit[2] + ", " + dateStringSplit[4];
 }
 
 
 function findAttackSpeed(row){
-    var TimeLeft = getTimeLeftInSecond(row);
-    var distance = getDistance(row);
-    var previousUnit = prevUnit;
+    const TimeLeft = getTimeLeftInSecond(row);
+    const distance = getDistance(row);
+    let previousUnit = prevUnit;
 
-    for(var unit in baseUnitSpeed){
+    for(const unit in baseUnitSpeed){
         if(getTravelTimeInSecond(distance, unit)<TimeLeft) return previousUnit;
         previousUnit = unit;
     }
-    return explor;
+    return explore;
 }
 
 
 function getImpactTime(row){
-    return  $(row).find('td')[5].innerText; // TODO A Testzer
+    return  $(row).find('td')[5].innerText; // TODO A Tester
 }
 
 function hasNumbers(t)
@@ -332,35 +213,41 @@ function hasNumbers(t)
 }
 
 
-function conversionImpacttoDate(row){
-    var impact = getImpactTime(row);
+function conversionImpactDate(row){
+    const impact = getImpactTime(row);
     console.log(impact + " oi");
-    var tab = impact.split(" ");
-    var dateActuelle = new Date();
+    const tab = impact.split(" ");
+    const dateActual = new Date();
     console.log(tab);
-    var last = tab[tab.length-1];
-    var index = tab.length-1;
+    const last = tab[tab.length - 1];
+    let index = tab.length - 1;
     if(!hasNumbers(last)){
         index = index-1;
     }
-
+    let dateCalculate;
     switch(tab[0]){
         case tomorrowTranslated:
-            return new Date(dateActuelle.getFullYear(), dateActuelle.getMonth(), parseInt(dateActuelle.getDate())+1, tab[index].split(":")[0], tab[index].split(":")[1], tab[index].split(":")[2]);
+            dateCalculate = new Date(dateActual.getFullYear(), dateActual.getMonth(), parseInt(dateActual.getDate())+1, tab[index].split(":")[0], tab[index].split(":")[1], tab[index].split(":")[2]);
+            break
         case atTranslated:
-            return new Date(tab[1].split(".")[2], parseInt(tab[1].split(".")[1])-1, tab[1].split(".")[0],tab[3].split(":")[0], tab[3].split(":")[1], tab[3].split(":")[2]);
+            dateCalculate = new Date(tab[1].split(".")[2], parseInt(tab[1].split(".")[1])-1, tab[1].split(".")[0],tab[3].split(":")[0], tab[3].split(":")[1], tab[3].split(":")[2]);
+            break
         default:
-            return new Date(dateActuelle.getFullYear(), dateActuelle.getMonth(), parseInt(dateActuelle.getDate()), tab[index].split(":")[0], tab[index].split(":")[1], tab[index].split(":")[2]);
+            dateCalculate = new Date(dateActual.getFullYear(), dateActual.getMonth(), parseInt(dateActual.getDate()), tab[index].split(":")[0], tab[index].split(":")[1], tab[index].split(":")[2]);
+            break
 
     }
-    throw new Error("oops, error in data conversion")
+    if(dateCalculate==null) {throw new Error("oops, error in data conversion")}
+
+    return dateCalculate;
+
 }
 
 function findAttackLaunch(row){
-    var impact = conversionImpacttoDate(row);
-    var travel = getTravelTimeInSecond(getDistance(row), findAttackSpeed(row));
-    var launchDate = new Date(impact.getFullYear(), impact.getMonth(), impact.getDate(), impact.getHours(), impact.getMinutes(), parseInt(impact.getSeconds())-travel)
-    var dateStringSplit = launchDate.toString().split(" ")
+    const impact = conversionImpactDate(row);
+    const travel = getTravelTimeInSecond(getDistance(row), findAttackSpeed(row));
+    const launchDate = new Date(impact.getFullYear(), impact.getMonth(), impact.getDate(), impact.getHours(), impact.getMinutes(), parseInt(impact.getSeconds()) - travel);
+    const dateStringSplit = launchDate.toString().split(" ");
     return dateStringSplit[1] + ". " + dateStringSplit[2] + ", " + dateStringSplit[4]
 }
 
@@ -371,21 +258,21 @@ function getFinalString(row){
 
 //----------
 function editAttackName($row) {
-    var $button = $row.find('.rename-icon');
+    const $button = $row.find('.rename-icon');
     $button.click();
 }
 function renameAttackName($row, name) {
-    var $input = $row.find('.quickedit-edit input[type="text"]');
+    const $input = $row.find('.quickedit-edit input[type="text"]');
     $input.val(name);
 }
 function submitAttackName($row) {
-    var $button = $row.find('.quickedit-edit input[type="button"]');
+    const $button = $row.find('.quickedit-edit input[type="button"]');
     $button.click();
 }
 
 
 function RenameAttack(){
-    var i=0;
+    let i = 0;
     $('tr.nowrap').each(function(){
         if ($.trim($(this).find('td')[0].innerText).includes(attackTranslated)){
             setTimeout(delayed, i*150,$(this));
